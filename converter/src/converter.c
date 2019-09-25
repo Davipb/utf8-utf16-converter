5338a5f07e204e51cda0964ba6221a4962896df9
Davipb <daviparca@gmail.com>
2019-09-24 22:42:06 -0300

    Initial Commit

---
+++ converter/src/converter.c
@@ -0,0 +1,21 @@
+#include <converter.h>
+#include <stdio.h>
+
+size_t utf16_to_utf8(
+    uint16_t const* utf16, size_t utf16_len, 
+    uint8_t* utf8,         size_t utf8_len
+) 
+{
+    fprintf(stderr, "NOT IMPLEMENTED YET");
+    return 0;
+}
+
+
+size_t utf8_to_utf16(
+    uint8_t const* utf8, size_t utf8_len, 
+    uint16_t* utf16,     size_t utf16_len
+) 
+{
+    fprintf(stderr, "NOT IMPLEMENTED YET");
+    return 0;
+}
---
+++ test/src/test.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "converter.h"
+
+
+int main(int argc, char* argv[]) 
+{
+    fprintf(stderr, "NOT IMPLEMENTED YET");
+    return EXIT_FAILURE;
+}
